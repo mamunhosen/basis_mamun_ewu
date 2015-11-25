0b0853e435619169e5a8da15e60431de531d8c94
hossain-mamun <hossainmamun035@gmail.com>
2015-11-25 10:03:09 +0600

    Shell comment

--- comment.php
+++ comment.php
@@ -9,10 +9,11 @@ $a=20;
 $b=10;
 $c=$a+$b;
 $d=$a-$b;
+$e=$a*$b;
 echo "Summation of a+b: ".$c.'<br/>'; //Summation.
 echo "Subtraction of a-b: ".$d.'<br/>';/* this is multiline comment.
 yet another line of comment*/
-
+echo "Multiplication of a*b: ".$e.'<br/>';# Shell style comment.
 
 ?>
 </body>
