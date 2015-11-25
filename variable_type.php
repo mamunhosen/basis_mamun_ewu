837cd1ea98824a6f592849bfe70ef8d2cdbb4201
hossain-mamun <hossainmamun035@gmail.com>
2015-11-25 10:56:43 +0600

    Associative Array

--- variable_type.php
+++ variable_type.php
@@ -13,8 +13,11 @@ $c=20.8; //float type
 echo $c.'<br/>';
 $d='Hossain Mamun'; //string type
 echo $d.'<br/>';
-$names = array('Mamun','shovon');
+$names = array('Mamun','shovon');//indexed array
 var_dump($names);
+echo "<br/>";
+$numbers=array('Mamun'=>90,'Shuvo'=>70);
+var_dump($numbers);
 
 ?>
 
