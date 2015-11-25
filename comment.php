cca43b849630e5965cec439d484ee3642802c3f2
hossain-mamun <hossainmamun035@gmail.com>
2015-11-25 09:51:55 +0600

    single line comment

---
+++ comment.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Different Comment</title>
+</head>
+<body>
+<?php
+$a=20;
+$b=10;
+echo "Summation of a+b: ".$a+$b.'<br/>'; //Summation.
+
+?>
+</body>
+</html>
