b8cc4522440325204942db2452e6233071da01fe
hossain-mamun <hossaimamun035@gmail.com>
2015-11-30 10:19:54 +0600

    use of magic constants line,file,function,class,method

--- 30-11-2015.php
+++ 30-11-2015.php
@@ -7,6 +7,25 @@
  <?php
 define("CONSTANT", "Hello world.");
 echo CONSTANT.'<br/>'; // outputs "Hello world."
+echo "line Number".__LINE__."<br/>";//magic constants
+echo "The directory of the file is : ".__FILE__.'<br/>';//magic constants;
+
+function Bitm(){
+	echo "The name of function is: ".__Function__.'<br/>';
+}
+ bitm();
+ class bitm{
+ 	public function php13(){
+    echo __CLASS__.'<br/>';
+    echo __METHOD__.'<br/>';
+
+
+ 	}
+ }
+
+ $welcome=new bitm();
+ $welcome->php13();
+ //use_of_trait
 
 
   
