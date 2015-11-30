81fb77d3ba0413e961a828500126d1bfbf59c6a9
hossain-mamun <hossaimamun035@gmail.com>
2015-11-30 10:16:10 +0600

    use of constants

---
+++ 30-11-2015.php
@@ -0,0 +1,15 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>30.11.2015</title>
+ </head>
+ <body>
+ <?php
+define("CONSTANT", "Hello world.");
+echo CONSTANT.'<br/>'; // outputs "Hello world."
+
+
+  
+?>
+ </body>
+ </html>
