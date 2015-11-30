d808b0fa9d30ea7ebb369f3a9362b372fcfbc34e
hossain-mamun <hossaimamun035@gmail.com>
2015-11-30 13:00:38 +0600

    expressions

--- 30-11-2015.php
+++ 30-11-2015.php
@@ -14,13 +14,13 @@ echo "line Number".__LINE__."<br/>";//magic constants
 echo "The directory of the file is : ".__FILE__.'<br/>';//magic constants to find the file directory;
 
 function Bitm(){
-	echo "The name of function is: ".__Function__.'<br/>';
+	echo "The name of function is: ".__Function__.'<br/>';//magic constant to find the function name;
 }
  bitm();
  class bitm{
  	public function php13(){
-    echo __CLASS__.'<br/>';
-    echo __METHOD__.'<br/>';
+    echo __CLASS__.'<br/>';//magic constant to find the class name;
+    echo __METHOD__.'<br/>';//magic constant to find the method name;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 
 
  	}
@@ -43,6 +43,26 @@ father_of_trait::greetings();
 echo "<br/>";
 
 \mamun\use_of_namespace::confused();//imported from another page and i used namespace
+//Expression:
+
+function mul($i){
+	return $i*2;
+
+}
+
+$a=5;
+$c=$a++;
+
+echo $c;
+$d=7;
+echo mul(++$d);
+$a=20;
+echo "<br/>";
+$c=$a---$a--;
+echo "the value is : $c";
+echo "<br/>";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+echo $a;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+
 ?>
  </body>
  </html>
