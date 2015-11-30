5803f71c452e41751788f48f829fff89de4543fd
hossain-mamun <hossaimamun035@gmail.com>
2015-11-30 10:45:21 +0600

    Use of Namespace

--- 30-11-2015.php
+++ 30-11-2015.php
@@ -5,10 +5,13 @@
  </head>
  <body>
  <?php
+ include 'namespace30.php';
+ 
 define("CONSTANT", "Hello world.");
+echo "<br/>";
 echo CONSTANT.'<br/>'; // outputs "Hello world."
 echo "line Number".__LINE__."<br/>";//magic constants
-echo "The directory of the file is : ".__FILE__.'<br/>';//magic constants;
+echo "The directory of the file is : ".__FILE__.'<br/>';//magic constants to find the file directory;
 
 function Bitm(){
 	echo "The name of function is: ".__Function__.'<br/>';
@@ -37,8 +40,9 @@ class father_of_trait{
 	use use_of_trait;
 }
 father_of_trait::greetings();
+echo "<br/>";
 
-  
+\mamun\use_of_namespace::confused();//imported from another page and i used namespace
 ?>
  </body>
  </html>
---
+++ namespace30.php
@@ -0,0 +1,19 @@
+<?php namespace mamun; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>namespace</title>
+</head>
+<body>
+<?php
+class use_of_namespace{
+
+	public function confused(){
+		echo "Hello Namespace";
+	}
+}
+?>
+	
+</body>
+</html>
