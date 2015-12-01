4caf2bdfce21346d5c5a5d496a5e19c79ab97438
hossain-mamun <hossainmamun035@gmail.com>
2015-12-01 19:42:19 +0600

    String Function

---
+++ stringFunction.php
@@ -0,0 +1,16 @@
+<?php
+//join function.
+//syntex join(separator,array);
+$join = array('Welcome','to','join','function' );
+echo join(' ',$join);
+//htmlentities function
+echo "<br/>";
+$str = "<© W3Sçh°°¦§>";
+echo htmlentities($str);
+//money_format function
+
+
+
+
+
+?>
