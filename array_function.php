2db4f13055d5a8d259dbce693d1a6b7404badd78
hossain-mamun <hossainmamun035@gmail.com>
2015-12-27 23:01:14 +0600

    array function

--- array_function.php
+++ array_function.php
@@ -14,10 +14,42 @@ else{
 	echo "not exist";
 }
 echo "<br/>";
-print_r(array_pad($b, 3, 'bvk'));
+echo "Array_pad: ";
+print_r(array_pad($b, 5, 'bvk'));
 echo "<br/>";
 $push=array_push($b,'porshe','boghati');
+echo "Array_push: ";
 print_r($b);
 
 echo "<br/>";
+$a=array("red","green","blue");
+array_pop($a);
+echo "Array_pop: ";
+print_r($a);
+echo "<br/>";
+$a=array("red","green","blue","yellow","brown");
+$random_keys=array_rand($a,4);
+echo "Array_rand: <br/>";
+echo $a[$random_keys[0]].'<br/>';
+echo $a[$random_keys[1]].'<br/>';
+echo $a[$random_keys[2]].'<br/>';
+echo $a[$random_keys[3]].'<br/>';
+echo "array_replace";
+$d=array('dhaka');
+$b=array('Ctg');
+$c=array('Khulna');
+print_r(array_replace($d,$b,$c));
+echo "<br/>";
+echo "array_search:";
+echo array_search('blue',$a);
+echo "<br/>";
+echo "array_reverse:";
+print_r(array_reverse($a));
+echo "<br/>";
+$age=array("Peter"=>"40","Ben"=>"37","Joe"=>"43");
+asort($age);
+echo "array_asort:";
+print_r($age);
+
+
 ?>
--- index.php
+++ index.php
@@ -12,6 +12,8 @@ and open the template in the editor.
     <body>
         <?php
        echo  "<h3>"."Hello Git"."</h3>";
+
         ?>
+       
     </body>
 </html>
